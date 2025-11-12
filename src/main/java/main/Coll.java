e65944f6a33c8556b54575d75aa6ed17259f823f
dipukr <adipukr@gmail.com>
2025-11-12 15:36:31 +0530

    change pom.xml

--- src/main/java/main/Coll.java
+++ src/main/java/main/Coll.java
@@ -4,11 +4,11 @@ import java.util.Scanner;
 
 public class Coll {
 	public static void main(String[] args) throws Exception {
+		Scanner scanner = new Scanner(System.in);
 		Answer<Integer> answer = Ok.of(10067);
 		switch (answer) {
 		case Ok<Integer> ok -> System.out.println("User: " + ok.value());
 		case Err<Integer> err -> System.err.println("Error: " + err.error());
 		}
-		Scanner scanner = new Scanner(System.in);
 	}
 }
